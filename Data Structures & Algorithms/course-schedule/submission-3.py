c51eec17f9948b1f7c334f4ced7dc08252fad139
haseeb <68353841+haseebhasan2019@users.noreply.github.com>
2026-06-16 13:25:49 -0700

    Add: course-schedule - submission-3

---
+++ Data Structures & Algorithms/course-schedule/submission-3.py
@@ -0,0 +1,25 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        in_deg = [0] * numCourses # num of prereqs for course i
+        graph = defaultdict(list) # course -> children
+        for course, prereq in prerequisites:
+            in_deg[course]+=1
+            graph[prereq].append(course)
+        # queue up classes with no prereqs
+        queue = deque()
+        visited = set()
+        for course in range(numCourses):
+            if in_deg[course] == 0:
+                queue.append(course)
+        # iterate through queue and decrease in-degs of children
+        while queue:
+            course = queue.popleft()
+            visited.add(course)
+            for child in graph[course]:
+                if in_deg[child] == 0:
+                    return False
+                in_deg[child]-=1
+                if in_deg[child] == 0:
+                    queue.append(child)
+        return len(visited) == numCourses
+        
