8c00e264365c6539761b15a207cd921e0a5c89dd
haseeb <68353841+haseebhasan2019@users.noreply.github.com>
2026-07-15 16:13:21 -0400

    Add: non-cyclical-number - submission-1

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        seen = set()
+        
+        def sum_of_squares(num):
+            res = 0
+            while num:
+                digit = num % 10
+                num //= 10
+                res += digit ** 2
+            return res
+
+        l = n
+        r = sum_of_squares(n)
+        while l != r:
+            if l == 1 or r == 1:
+                return True
+            l = sum_of_squares(l)
+            r = sum_of_squares(r)
+            r = sum_of_squares(r)
+        return l == 1 or r == 1
