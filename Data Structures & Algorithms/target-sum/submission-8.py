4faea34002bf0eb5500949df63c4b4fecce866c7
haseeb <68353841+haseebhasan2019@users.noreply.github.com>
2026-07-22 16:20:23 -0700

    Add: target-sum - submission-8

---
+++ Data Structures & Algorithms/target-sum/submission-8.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        memo = defaultdict(int)
+
+        def backtrack(i, curr_sum):
+            if i == len(nums):
+                if curr_sum == target:
+                    return 1
+                return 0
+            if (i, curr_sum) in memo:
+                return memo[(i, curr_sum)]
+            memo[(i, curr_sum)] += backtrack(i+1, curr_sum + nums[i])
+            memo[(i, curr_sum)] += backtrack(i+1, curr_sum - nums[i])
+            return memo[(i, curr_sum)]
+        
+        return backtrack(0, 0)
+
+# bt(0, 0) = 3
+#     bt(1, 2) -> 1
+#     bt(1, 0) = 2
+#         bt(2, 2) -> 1
+#         bt(2, 0)
+#             bt(3,2) -> 1
