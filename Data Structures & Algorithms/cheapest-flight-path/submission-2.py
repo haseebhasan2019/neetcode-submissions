f258909445515df48ef2445e79798f9aaebfa0c3
haseeb <68353841+haseebhasan2019@users.noreply.github.com>
2026-07-22 13:22:00 -0700

    Add: cheapest-flight-path - submission-2

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        # Bellman-Ford O(E * k)
+        prices = [float('inf')] * n
+        prices[src] = 0
+
+        # Iterate k+1 times (relaxes k+1 times)
+        for _ in range(k+1):
+            # Copy prices array
+            temp = prices.copy()
+            # Iterate through all edges and see if we can relax prices
+            for fro, to, price in flights:
+                if prices[fro] != float('inf'):
+                    temp[to] = min(temp[to], prices[fro] + price)
+            # set prices to copy
+            prices = temp
+
+        return prices[dst] if prices[dst] != float('inf') else -1
