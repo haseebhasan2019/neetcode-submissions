2fea661f9d05a499254c059ddbdb3b6969d20ed0
haseeb <68353841+haseebhasan2019@users.noreply.github.com>
2026-08-10 13:29:15 -0700

    Add: coin-change - submission-2

---
+++ Data Structures & Algorithms/coin-change/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        if amount == 0:
+            return 0
+
+        arr = [0] * (amount + 1)
+
+        for target in range(len(arr)):
+            for coin in coins:
+                if coin == target:
+                    arr[target] = 1
+                elif target > coin and arr[target-coin] > 0:
+                    if arr[target] == 0:
+                        arr[target] = arr[target-coin] + 1
+                    else:
+                        arr[target] = min(arr[target], arr[target-coin] + 1)
+        return arr[amount] if arr[amount] > 0 else -1
+
+# coins=[1,2,5]
+# amount=11
+# [0  1  1  2  2  1  2  2  3  3  2  3]
+#  0  1  2  3  4  5  6  7  8  9  10 11 
+# Have an array from 0 to amount where each element in the array represents the fewest number of coins needed to make that amount
+
+# For each index in amount, iterate through all coins and make it equal to the min of that
