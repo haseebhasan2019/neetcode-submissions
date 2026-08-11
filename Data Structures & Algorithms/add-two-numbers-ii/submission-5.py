0d520696543c30deab396732858f5e4e8da7c507
haseeb <68353841+haseebhasan2019@users.noreply.github.com>
2026-08-11 09:55:00 -0700

    Add: add-two-numbers-ii - submission-5

---
+++ Data Structures & Algorithms/add-two-numbers-ii/submission-5.py
@@ -0,0 +1,43 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        def reverse_ll(head):
+            rev = None
+            while head:
+                node = head
+                head = head.next
+                node.next = rev
+                rev = node
+            return rev
+
+        # Create stacks of l1 and l2
+        stk1 = []
+        ptr = l1
+        while ptr:
+            stk1.append(ptr.val)
+            ptr = ptr.next
+        stk2 = []
+        ptr = l2
+        while ptr:
+            stk2.append(ptr.val)
+            ptr = ptr.next
+        # Process stacks
+        res = None
+        carry = 0
+
+        while stk1 or stk2 or carry:
+            val = carry
+            if stk1:
+                val += stk1.pop()
+            if stk2:
+                val += stk2.pop()
+            carry, digit = divmod(val, 10)
+            node = ListNode(digit)
+            node.next = res
+            res = node
+
+        return res
